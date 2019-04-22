606942106a8c680ba61bd78d46d1eeb55ec20f0c
yu840915 <yu840915@gmail.com>
2019-04-22 14:06:49 +0800

    Add shared configs

---
+++ Oneplanet/SharedFormatters.swift
@@ -0,0 +1,19 @@
+//
+//  SharedFormatters.swift
+//  Oneplanet
+//
+//  Created by 立宣于 on 2019/4/22.
+//  Copyright © 2019 何一品居. All rights reserved.
+//
+
+import Foundation
+
+class SharedNumberFormatters {
+    static let integer: NumberFormatter = {
+        let value = NumberFormatter()
+        value.numberStyle = .decimal
+        value.maximumFractionDigits = 0
+        value.roundingMode = .up
+        return value
+    }()
+}

80c4196ed3af8d053bd9a91c1ef5271bd1465a2d
yu840915 <yu840915@gmail.com>
2019-04-22 15:16:51 +0800

    Rename to root view controller

--- Oneplanet/RootViewController.swift
+++ Oneplanet/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  Oneplanet
 //
 //  Created by 立宣于 on 2019/4/22.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

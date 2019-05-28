edfff7499ad39ba5ba604acf0aed7a29a12bb3c6
yu840915 <yu840915@gmail.com>
2019-05-28 14:30:22 +0800

    Add promo popup

---
+++ Oneplanet/PromoPopUpFlowViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PromoPopUpFlowViewController.swift
+//  Oneplanet
+//
+//  Created by 立宣于 on 2019/5/28.
+//  Copyright © 2019 何一品居. All rights reserved.
+//
+
+import UIKit
+
+class PromoPopUpFlowViewController: UIViewController {
+
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Oneplanet/PromoPopUpViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PromoPopUpViewController.swift
+//  Oneplanet
+//
+//  Created by 立宣于 on 2019/5/28.
+//  Copyright © 2019 何一品居. All rights reserved.
+//
+
+import UIKit
+
+class PromoPopUpViewController: UIViewController {
+
+    @IBOutlet weak var goButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func goToLink(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func exit(_ sender: UIButton) {
+        
+    }
+}

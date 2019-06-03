2c731ac79c126131f2e1873f605e32e68de81189
yu840915 <yu840915@gmail.com>
2019-06-03 16:49:49 +0800

    Level up animation

--- Oneplanet/TreasuryBarViewController.swift
+++ Oneplanet/TreasuryBarViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class TreasuryBarViewController: UIViewController {
 
+    @IBOutlet weak var blueGemIcon: UIImageView!
+    @IBOutlet weak var purpleGemIcon: UIImageView!
+    @IBOutlet weak var greenGemIcon: UIImageView!
+    
+    @IBOutlet weak var scorebarButton: UIButton!
     @IBOutlet var treasuryButtons: [UIButton]!
     @IBOutlet weak var scorebarBackImage: UIImageView!
     @IBOutlet weak var scoreMaskView: UIView!
@@ -25,11 +30,25 @@ class TreasuryBarViewController: UIViewController {
             button.imageView?.layer.shadowOpacity = 1.0
             button.imageView?.clipsToBounds = false
         }
-        scorebarBackImage.layer.shadowColor = #colorLiteral(red: 0.3725490196, green: 0.8235294118, blue: 0.8078431373, alpha: 1)
-        scorebarBackImage.layer.shadowRadius = 4
-        scorebarBackImage.layer.shadowOffset = .zero
-        scorebarBackImage.layer.shadowOpacity = 1.0
+        scorebarButton.layer.shadowColor = #colorLiteral(red: 0.3725490196, green: 0.8235294118, blue: 0.8078431373, alpha: 1)
+        scorebarButton.layer.shadowRadius = 4
+        scorebarButton.layer.shadowOffset = .zero
+        scorebarButton.layer.shadowOpacity = 1.0
         scoreMaskView.layer.cornerRadius = 5.0
+    }
+    
+    private func animateLevelUp(for view: UIView, delay: TimeInterval = 0.0) {
+        view.alpha = 1.0
+        UIView.animate(withDuration: 1.0, delay: delay, options: [.curveLinear], animations: {
+            view.transform = CGAffineTransform(scaleX: 30, y: 30).translatedBy(x: 0, y: 1)
+            view.alpha = 0
+        }) { (_) in
+            view.transform = .identity
+            view.alpha = 0.0
+        }
+    }
+    @IBAction func animatePurpleGen(_ sender: UIButton) {
+        animateLevelUp(for: purpleGemIcon, delay: 0.0)
     }
     
     /*

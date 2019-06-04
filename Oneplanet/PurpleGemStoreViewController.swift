9f5592826b9f520c6ee25359cb67779d23d83f4d
yu840915 <yu840915@gmail.com>
2019-06-04 16:58:10 +0800

    Purple gem pop up

---
+++ Oneplanet/PurpleGemStoreViewController.swift
@@ -0,0 +1,62 @@
+//
+//  PurpleGemStoreViewController.swift
+//  Oneplanet
+//
+//  Created by 立宣于 on 2019/6/4.
+//  Copyright © 2019 何一品居. All rights reserved.
+//
+
+import UIKit
+
+class PurpleGemStoreViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var usageLabel: UILabel!
+    @IBOutlet weak var bulletTextView1: UITextView!
+    @IBOutlet weak var bulletTextView2: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        localizeTitles()
+    }
+    
+    private func localizeTitles() {
+        nameLabel.text = Localized.titles.purpleGem
+        usageLabel.text = Localized.gemStonePopUp.purpleGemUsage
+        bulletTextView2.text = Localized.gemStonePopUp.purpleGemBullet2
+        prepareActionBullet()
+    }
+
+    private func prepareActionBullet() {
+        let text = String(format: Localized.gemStonePopUp.purpleGemBullet1, Localized.titles.blueGem)
+        let linkRange = (text as NSString).range(of: Localized.titles.blueGem)
+        let attrStr = NSMutableAttributedString(string: text, attributes: [.font:  UIFont.systemFont(ofSize: 12)])
+        attrStr.addAttributes([.link : DeepLinks.blueGemPopUp, .font: UIFont.systemFont(ofSize: 12, weight: .semibold)], range: linkRange)
+        bulletTextView1.delegate = self
+        bulletTextView1.attributedText = attrStr
+        bulletTextView1.linkTextAttributes = [
+            .foregroundColor : ColorPalette.buttonGreen,
+            .font: UIFont.systemFont(ofSize: 12, weight: .semibold)]
+    }
+    
+    @IBAction func exit(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+
+}
+
+extension PurpleGemStoreViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        OperationQueue.main.addOperation {
+            self.dismiss(animated: true, completion: {
+                router.handle(URL)
+            })
+        }
+        return false
+    }
+}

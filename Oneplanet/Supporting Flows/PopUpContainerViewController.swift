2f5ccae080c2507cfc93dc1e9f01375b3c68af25
yu840915 <yu840915@gmail.com>
2019-06-06 18:48:48 +0800

    Add tap to dismiss to popup

--- Oneplanet/Supporting Flows/PopUpContainerViewController.swift
+++ Oneplanet/Supporting Flows/PopUpContainerViewController.swift
@@ -13,13 +13,39 @@ class PopUpContainerViewController: UIViewController {
     private(set) var contentViewController: UIViewController!
     var contentViewControllerSetUpBlock: ((UIViewController)->())?
     var shouldAddConstraintToContentView = false
+    var shouldAddConstraintToDismissView = false
     @IBOutlet weak var contentContainer: UIView!
+    private var dismissTap: UITapGestureRecognizer!
+    private var dismissView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         contentContainer.layer.cornerRadius = 8
         shouldAddConstraintToContentView = true
+        setUpDismissTap()
         updateViewConstraints()
+    }
+    
+    private func setUpDismissTap() {
+        let dismiss = UIView()
+        dismiss.backgroundColor = .clear
+        view.addSubview(dismiss)
+        view.sendSubviewToBack(dismiss)
+        dismissView = dismiss
+        let tap = UITapGestureRecognizer(target: self, action: #selector(exit(_:)))
+        tap.isEnabled = false
+        dismiss.addGestureRecognizer(tap)
+        dismissTap = tap
+        shouldAddConstraintToDismissView = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        dismissTap.isEnabled = true
+    }
+    
+    @IBAction func exit(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
     override func updateViewConstraints() {
@@ -30,6 +56,13 @@ class PopUpContainerViewController: UIViewController {
             let views = ["content": content]
             container.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[content]|", options: [], metrics: nil, views: views))
             container.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[content]|", options: [], metrics: nil, views: views))
+        }
+        if shouldAddConstraintToDismissView {
+            let content = dismissView!
+            content.translatesAutoresizingMaskIntoConstraints = false
+            let views = ["content": content]
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[content]|", options: [], metrics: nil, views: views))
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[content]|", options: [], metrics: nil, views: views))
         }
         super.updateViewConstraints()
     }

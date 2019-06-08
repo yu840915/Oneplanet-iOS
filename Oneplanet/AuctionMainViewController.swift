41af293bb7603c01e9945de4a9424400c0614f39
yu840915 <yu840915@gmail.com>
2019-06-08 18:18:05 +0800

    Add badge view

--- Oneplanet/AuctionMainViewController.swift
+++ Oneplanet/AuctionMainViewController.swift
@@ -12,6 +12,8 @@ import XLPagerTabStrip
 class AuctionMainViewController: ButtonBarPagerTabStripViewController {
 
     @IBOutlet weak var buttonBarContainer: UIView!
+    private var shouldAddBadgeViews = true
+    private var biddingProcessBadge: BadgeView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -23,6 +25,22 @@ class AuctionMainViewController: ButtonBarPagerTabStripViewController {
         changeCurrentIndexProgressive = {[weak self] (oldCell, newCell, progressPercentage, changeCurrentIndex, animated) in
             self?.updateButtonBarCell(oldCell: oldCell, newCell: newCell, progressPercentage: progressPercentage, changeCurrentIndex: changeCurrentIndex, animated: animated)
         }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setUpBadgeViewIfNeeded()
+    }
+    
+    private func setUpBadgeViewIfNeeded() {
+        guard biddingProcessBadge == nil else { return }
+        let badge = BadgeView.fromDefaultNib()
+        let cell = buttonBarView.visibleCells.first as! ButtonBarViewCell
+        cell.addSubview(badge)
+        let label = cell.label!
+        cell.addConstraint(NSLayoutConstraint(item: label, attribute: .right, relatedBy: .equal, toItem: badge, attribute: .centerX, multiplier: 1, constant: -5))
+        cell.addConstraint(NSLayoutConstraint(item: label, attribute: .top, relatedBy: .equal, toItem: badge, attribute: .centerY, multiplier: 1, constant: 2))
+        biddingProcessBadge = badge
     }
     
     private func updateButtonBarCell(oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) {

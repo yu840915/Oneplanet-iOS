203561f663cd9433e2f3a5a1f8d7379df07d7f60
yu840915 <yu840915@gmail.com>
2019-10-04 21:05:07 +0800

    Lot list for admin

--- Oneplanet/MyLotList.swift
+++ Oneplanet/MyLotList.swift
@@ -53,7 +53,7 @@ class GetMyLotPageOperation: AlamofireAPIAccessOperation, PaginatedFetchingOpera
     
     convenience init(session: UserSession) {
         if session.isAdmin {
-            self.init(session: session, url: ServiceURLs.base.appendingPathComponent("products"), isBeginning: true)
+            self.init(session: session, url: ServiceURLs.base.appendingPathComponent("products/category/all"), isBeginning: true)
         } else {
             self.init(session: session, url: ServiceURLs.base.appendingPathComponent("products/unlocks"), isBeginning: true)
         }
